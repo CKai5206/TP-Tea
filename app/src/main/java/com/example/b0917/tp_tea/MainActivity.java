f898659642f45e5a622f74636f596798ebc48512
d0580785 <b09170915@gmail.com>
2018-05-04 10:40:37 +0800

    5/4
    Try fragment

--- app/src/main/java/com/example/b0917/tp_tea/MainActivity.java
+++ app/src/main/java/com/example/b0917/tp_tea/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,18 +18,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
         private Button menu_btn,branches_btn,event_btn,game_btn;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        frameLayout = findViewById(R.id.content);
+        FragmentManager frgMgr = getSupportFragmentManager();
+        FragmentTransaction frgTranc = frgMgr.beginTransaction();
 
+        // change page
+        frgTranc.add(new content_main(), "content");
+//        frgTranc.replace(R.id.content, new menu(), "fragmentTag");
+
+        frgTranc.commit();
+        frgMgr.executePendingTransactions();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -36,43 +51,50 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        getButtonView();
-        setButtonEvent();
+
     }
 
-    private void getButtonView(){
-        menu_btn = (Button)findViewById(R.id.menu_btn);
-        branches_btn = (Button)findViewById(R.id.branches_btn);
-        event_btn = (Button)findViewById(R.id.event_btn);
-        game_btn = (Button)findViewById(R.id.game_btn);
-    }
+//    private void getButtonView(){
+//        menu_btn = (Button)findViewById(R.id.menu_btn);
+//        branches_btn = (Button)findViewById(R.id.branches_btn);
+//        event_btn = (Button)findViewById(R.id.event_btn);
+//        game_btn = (Button)findViewById(R.id.game_btn);
+//    }
+//
+//    public void setButtonEvent(){
+//        menu_btn.setOnClickListener(buttonListener);
+//        branches_btn.setOnClickListener(buttonListener);
+//        event_btn.setOnClickListener(buttonListener);
+//        game_btn.setOnClickListener(buttonListener);
+//    }
 
-    public void setButtonEvent(){
-        menu_btn.setOnClickListener(buttonListener);
-        branches_btn.setOnClickListener(buttonListener);
-        event_btn.setOnClickListener(buttonListener);
-        game_btn.setOnClickListener(buttonListener);
-    }
-
-    private Button.OnClickListener buttonListener= new Button.OnClickListener(){
-        @Override
-        public void onClick(View view){
-            switch (view.getId()){
-                case R.id.menu_btn:
-                    Toast.makeText(MainActivity.this,"menu",Toast.LENGTH_SHORT).show();
-                break;
-                case R.id.branches_btn:
-                    Toast.makeText(MainActivity.this,"branches",Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.event_btn:
-                    Toast.makeText(MainActivity.this,"event",Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.game_btn:
-                    Toast.makeText(MainActivity.this,"game",Toast.LENGTH_SHORT).show();
-                break;
-            }
-        }
-    }
+//    private Button.OnClickListener buttonListener= new Button.OnClickListener(){
+//        @Override
+//        public void onClick(View view){
+//            FragmentManager fragmentMgr = getSupportFragmentManager();
+//            FragmentTransaction fragmentTrans = fragmentMgr.beginTransaction();
+//            Fragment fragment_menu = new menu();
+//;
+//
+//             switch (view.getId()){
+//                case R.id.menu_btn:;
+//                    fragmentTrans.add(fragment_menu, "fragmentTag");
+//                    fragmentTrans.replace(R.id.content_frame, fragment_menu, "fragmentTag");
+//                    fragmentTrans.commit();
+//                    Log.v("frame", ""+fragmentMgr.executePendingTransactions());
+//                break;
+//                case R.id.branches_btn:
+//                    Toast.makeText(MainActivity.this,"branches",Toast.LENGTH_SHORT).show();
+//                    break;
+//                case R.id.event_btn:
+//                    Toast.makeText(MainActivity.this,"event",Toast.LENGTH_SHORT).show();
+//                    break;
+//                case R.id.game_btn:
+//                    Toast.makeText(MainActivity.this,"game",Toast.LENGTH_SHORT).show();
+//                break;
+//            }
+//        }
+//    };
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
