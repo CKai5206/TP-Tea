a7d3f8e2936564005304ea9b7cedcd9e92acc4d2
d0580785 <b09170915@gmail.com>
2018-05-08 14:22:35 +0800

    Revert "5/4"
    This reverts commit f898659642f45e5a622f74636f596798ebc48512.

---
+++ app/src/main/java/com/example/b0917/tp_tea/menu_page.java
@@ -0,0 +1,13 @@
+package com.example.b0917.tp_tea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menu_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_page);
+    }
+}
