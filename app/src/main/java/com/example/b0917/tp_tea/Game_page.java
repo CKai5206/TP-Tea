0143a87a9c0e3efe579b5bc90caf0a0e56758ccf
d0542218 <32465113+d0542218@users.noreply.github.com>
2018-05-31 20:06:38 +0800

    img
    img

--- app/src/main/java/com/example/b0917/tp_tea/Game_page.java
+++ app/src/main/java/com/example/b0917/tp_tea/Game_page.java
@@ -9,7 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Game_page extends Fragment{
+
     View inflatedView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -20,5 +28,6 @@ public class Game_page extends Fragment{
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
 }
