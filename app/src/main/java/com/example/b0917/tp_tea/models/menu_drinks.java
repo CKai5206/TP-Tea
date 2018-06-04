4d17ffbade47f09d05aff0af6814a8953925f128
d0580785 <b09170915@gmail.com>
2018-06-04 10:12:14 +0800

    create menu_event class

---
+++ app/src/main/java/com/example/b0917/tp_tea/models/menu_drinks.java
@@ -0,0 +1,13 @@
+package com.example.b0917.tp_tea.models;
+
+public class menu_drinks {
+    public String name;
+    public double price;
+    public boolean hot;
+
+    public menu_drinks(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.hot = false;
+    }
+}
