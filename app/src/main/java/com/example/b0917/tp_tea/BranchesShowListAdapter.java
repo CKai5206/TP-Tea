533d30f309c1e34c2902bf8e40fa8fe10e728dfd
hidochunk <32110358+hidochunk@users.noreply.github.com>
2018-06-07 20:23:30 +0800

    註解等下在加
    好麻煩

--- app/src/main/java/com/example/b0917/tp_tea/BranchesShowListAdapter.java
+++ app/src/main/java/com/example/b0917/tp_tea/BranchesShowListAdapter.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -20,11 +22,14 @@ public class BranchesShowListAdapter extends BaseAdapter {
     private LayoutInflater myInflater;
     private List<BranchData.Branch> branches;
     private Context context;
+    int currentItem = -1;
+    float scale ;
 
     public BranchesShowListAdapter(Context context, List<BranchData.Branch> branches){
         myInflater = LayoutInflater.from(context);
         this.branches = branches;
         this.context = context;
+        scale = context.getResources().getDisplayMetrics().density;
     }
 
     @Override
@@ -46,16 +51,22 @@ public class BranchesShowListAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
 
+
         //set holder
         if(convertView==null){
             convertView = myInflater.inflate(R.layout.branche_list_view, null);
             holder = new ViewHolder(
+                    (RelativeLayout) convertView.findViewById(R.id.showArea),
+                    (ImageView) convertView.findViewById(R.id.openORclose),
                     (TextView) convertView.findViewById(R.id.storeName),
                     (TextView) convertView.findViewById(R.id.phone),
-                    (ImageButton) convertView.findViewById(R.id.showMapButton)
+                    (ImageButton) convertView.findViewById(R.id.showMapButton),
+                    (RelativeLayout) convertView.findViewById(R.id.hideArea),
+                    (TextView) convertView.findViewById(R.id.address)
             );
             convertView.setTag(holder);
         }else{
+            Log.i("convertView != null","holder = (ViewHolder) convertView.getTag();");
             holder = (ViewHolder) convertView.getTag();
         }
 
@@ -66,11 +77,46 @@ public class BranchesShowListAdapter extends BaseAdapter {
             convertView.setBackgroundColor(Color.parseColor("#10303030"));
         }
 
+        holder.showArea.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //用 currentItem 记录点击位置
+                Log.i("start click cell","START");
+                int tag = (Integer) view.getTag();
+                if (tag == currentItem) { //再次点击
+                    currentItem = -1; //给 currentItem 一个无效值
+                } else {
+                    currentItem = tag;
+                }
+                //通知adapter数据改变需要重新加载
+                notifyDataSetChanged(); //必须有的一步
+                Log.i("end click cell","END");
+            }
+        });
         final BranchData.Branch branch = (BranchData.Branch) getItem(position);
 
+        holder.showArea.setTag(position);
         holder.storeNmae.setText(branch.getStoreName());
         holder.phone.setText(branch.getPhoneNum());
         holder.showMapButton.setImageResource(R.drawable.ic_maptag);
+        holder.address.setText(branch.getAddress());
+        holder.openORcloce.setImageResource(R.drawable.ic_open);
+
+
+        if (holder.address.getText().length() <= 13){
+            holder.hideArea.getLayoutParams().height = (int) (23 * scale);
+        }else{
+            holder.hideArea.getLayoutParams().height = (int) (45 * scale);
+        }
+
+        if (currentItem == position) {
+            holder.hideArea.setVisibility(View.VISIBLE);
+            holder.openORcloce.setImageResource(R.drawable.ic_cloce);
+        } else {
+            holder.hideArea.setVisibility(View.GONE);
+            holder.openORcloce.setImageResource(R.drawable.ic_open);
+        }
 
         //set showMapButton onClick
         holder.showMapButton.setOnClickListener(new View.OnClickListener() {
@@ -86,14 +132,23 @@ public class BranchesShowListAdapter extends BaseAdapter {
     }
 
     private class ViewHolder {
+        RelativeLayout showArea;
+        ImageView openORcloce;
         TextView storeNmae;
         TextView phone;
         ImageButton showMapButton;
 
-        public ViewHolder(TextView storeNmae, TextView phone, ImageButton showMapButton){
+        RelativeLayout hideArea;
+        TextView address;
+
+        public ViewHolder(RelativeLayout showArea, ImageView openORcloce, TextView storeNmae, TextView phone, ImageButton showMapButton, RelativeLayout hideArea,  TextView address){
+            this.showArea = showArea;
+            this.openORcloce = openORcloce;
             this.storeNmae = storeNmae;
             this.phone = phone;
             this.showMapButton = showMapButton;
+            this.hideArea = hideArea;
+            this.address = address;
         }
     }
 }
